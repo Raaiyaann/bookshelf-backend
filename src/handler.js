23968005095ffbfbd71eadde14d38c7abe64a51d
Raaiyaann <moh.raiyan99@gmail.com>
2025-06-30 16:19:43 +0800

    menambahkan route, handler, dan peyimpanan buku

--- src/handler.js
+++ src/handler.js
@@ -13,18 +13,27 @@ const addBooksHandler = (request, h) => {
     readPage, // number
     reading, // boolead
   } = request.payload;
-
-  let finished = Boolean;
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  }
+  const finished = pageCount === readPage;
   const id = nanoid(16);
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
-
-  if (pageCount === readPage) {
-    finished = true;
-  } else {
-    finished = false;
-  }
-
   const newBooks = {
     id,
     name,
@@ -40,7 +49,6 @@ const addBooksHandler = (request, h) => {
     updatedAt,
   };
   booksShelf.push(newBooks);
-
   const isSuccess = booksShelf.filter((book) => book.id === id).length > 0; // menyimpan sebuah kondisi jika data benar benar masuk ke array notes. kalau iya maka true
 
   if (isSuccess) {
@@ -54,17 +62,110 @@ const addBooksHandler = (request, h) => {
     response.code(201);
     return response;
   }
+};
+
+// menampilkan seluruh buku (hanya 3 properti saja ditampilkan)
+const getAllBooksHandler = (request, h) => {
+  const books = booksShelf.map((book) => ({
+    // ini nanti artinya setiap data dari bookShelf di mapping, lalu data tertentu dikases menggunakan objek book
+    id: book.id,
+    name: book.name,
+    publisher: book.publisher,
+  }));
+  const response = h.response({
+    status: "success",
+    data: {
+      books,
+    },
+  });
+  response.code(200);
+  return response;
+};
+
+// menampilkan buku secara detail
+const getBooksByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const book = booksShelf.find((b) => b.id === id); // kalau misalkan tidak ada buku dalam booksShelf, nanti nilai yang dikembalikan undefined, maka cara menanganinya seperti dibawah
+  if (book !== undefined) {
+    return {
+      status: "success",
+      data: {
+        book, // menampilakan data buku yang id nya sudah di deteksi sama dengan dengan parameter request pada fungsi filter
+      },
+    };
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Buku tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
+// update buku
+const updateBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const {
+    name,
+    year, // number
+    author, // string
+    summary, // string
+    publisher, // string
+    pageCount, // number
+    readPage, // number
+    reading, // boolead
+  } = request.payload;
   if (readPage > pageCount) {
     const response = h.response({
-        status:"fail",
-        message:"Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount"
+      status: "fail",
+      message:
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  }
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  }
+
+  const index = booksShelf.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    const updatedAt = new Date().toISOString();
+    const finished = pageCount === readPage;
+    booksShelf[index] = {
+      ...booksShelf[index],
+      name,
+      year, // number
+      author, // string
+      summary, // string
+      publisher, // string
+      pageCount, // number
+      readPage, // number
+      reading, // boolead
+      finished,
+      updatedAt,
+    };
+    return h.response({
+      status: "success",
+      message: "Buku berhasil diperbarui",
     });
   }
   const response = h.response({
     status: "fail",
-    message: "Gagal Menambahkan Buku. Mohon isi Nama Buku",
+    message: "Gagal memperbarui buku. Id tidak ditemukan",
   });
-  response.code(500);
+  response.code(404);
   return response;
 };
-export { addBooksHandler };
+export {
+  addBooksHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  updateBookByIdHandler,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,29 @@
-import { addBooksHandler } from "./handler.js";
+import {
+  addBooksHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  updateBookByIdHandler,
+} from "./handler.js";
 const routes = [
   {
     method: "POST",
     path: "/books",
     handler: addBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler,
+  },
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: updateBookByIdHandler,
   },
 ];
 
